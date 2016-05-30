1520782f0311d256625ada0f7bfe9b1a10ff6c1f
shtgames <shtgamessts@gmail.com>
2016-05-30 18:30:07 +0300

    v1.3.2b

--- Frontend/RegionPanel.cpp
+++ Frontend/RegionPanel.cpp
@@ -24,9 +24,9 @@ namespace fEnd
 		BuildingLevels(const BuildingLevels& copy) = default;
 		BuildingLevels(BuildingLevels&& temp) = default;
 		BuildingLevels(const std::string& building)
-			: queued(gui::Icon(Resources::texture("building_bar_bg"), true), gui::Icon(Resources::texture("queued_building_count"), true)),
-			damaged(gui::Icon(Resources::texture("building_bar_bg"), true), gui::Icon(Resources::texture("damaged_building_levels"), true)),
-			built(gui::Icon(Resources::texture("building_bar_bg"), true), gui::Icon(Resources::texture("building_levels"), true)),
+			: queued(gui::Icon(Resources::texture("building_bar_bg")), gui::Icon(Resources::texture("queued_building_count"))),
+			damaged(gui::Icon(Resources::texture("building_bar_bg")), gui::Icon(Resources::texture("damaged_building_levels"))),
+			built(gui::Icon(Resources::texture("building_bar_bg")), gui::Icon(Resources::texture("building_levels"))),
 			build(gui::Icon(Resources::texture("prov_build_" + building), true))
 		{
 			queued.setUpdateFunction([building]() -> float
@@ -34,19 +34,29 @@ namespace fEnd
 				if (Map::target)
 					return (bEnd::Region::get(*Map::target).getBuildingLevels(building).second + bEnd::Region::get(*Map::target).getQueuedCount(building)) / 10.0f;
 				else return 0;
-			});
+			})
+				.setFillMessage(gui::HoverMessage(gui::bind("") + [building]()
+					{
+						if (!Map::target) return gui::bind("");
+						const auto levels(bEnd::Region::get(*Map::target).getBuildingLevels(building));
+						return levels.first < levels.second ? gui::bind("This building has been damaged and is currently at ") + gui::bind(std::to_string(levels.first), sf::Color::Yellow) +
+							gui::bind(".\nIt is built up to level ") + gui::bind(std::to_string(levels.second), sf::Color::Yellow) + gui::bind(" but will take time to repair.\nFurther combat may damage it even more.")
+							: (levels.second ? gui::bind("This building is built up to level ") + gui::bind(std::to_string(levels.second), sf::Color::Yellow) + gui::bind(".") : gui::bind("")) +
+							(bEnd::Region::get(*Map::target).getQueuedCount(building) ? gui::bind(" Currently building more.") : gui::bind(""));
+					}, Resources::font("arial"), 13));
 			damaged.setUpdateFunction(std::bind(updateFunction, building, 1));
 			built.setUpdateFunction(std::bind(updateFunction, building, 0));
 			build.bindAction(gui::Released, [building]()
 			{
 				if (!Map::target) return;
 				bEnd::Production::get(bEnd::Nation::player).addProductionItem(building, *Map::target);
-			});
-			build.setPredicates(gui::Button::PredicateArray{ [building]()
+			})
+				.setPredicates(gui::Button::PredicateArray{ [building]()
 			{
 				return Map::target && bEnd::Region::get(*Map::target).getController() == bEnd::Nation::player &&
 					bEnd::Region::get(*Map::target).getBuildingLevels(building).second + bEnd::Region::get(*Map::target).getQueuedCount(building) < 10;
-			} });
+			} })
+				.setMessage(gui::HoverMessage(gui::bind(building), Resources::font("arial"), 13));
 		}
 
 		std::unique_ptr<gui::Interactive> copy()const override
@@ -77,7 +87,7 @@ namespace fEnd
 
 		const bool input(const sf::Event& event)
 		{
-			return build.input(event) || built.input(event) || damaged.input(event) || queued.input(event);
+			return build.input(event) || queued.input(event) || built.input(event) || damaged.input(event);
 		}
 
 	private:
